59f2dd8ada7e0871464b77040eb64f26353c5e5f
Vaka37 <vak-forever@inbox.ru>
2024-03-28 03:23:15 +0300

    makeTextFields

--- Sources/TextFieldItem/TextFieldItem.swift
+++ Sources/TextFieldItem/TextFieldItem.swift
@@ -3,21 +3,33 @@
 
 import UIKit
 public class AutorizationTextField: UITextField {
-    public func makeLoginTextField(placeholder: String, borderColor: CGColor, leftIcon: UIImage) -> UITextField {
-        self.placeholder = placeholder
-        self.backgroundColor = .white
-        self.layer.borderWidth = 1
-        self.layer.cornerRadius = 12
-        self.layer.borderColor = borderColor
-        let leftView = UIView()
-        let imageView = UIImageView()
-        leftView.addSubview(imageView)
-        leftView.frame.size = CGSize(width: 30, height: 30)
-        imageView.frame.size = CGSize(width: 18, height: 18)
-        imageView.center = leftView.center
-        imageView.image = leftIcon
-        self.leftView = leftView
-        self.leftViewMode = .always
-        return self
-    }
+    public struct Model {
+          public var placeholder: String?
+          public init(placeholder: String?) {
+              self.placeholder = placeholder
+          }
+      }
+
+      public override init(frame: CGRect) {
+          super.init(frame: frame)
+          setupField()
+      }
+      
+      required init?(coder: NSCoder) {
+          super.init(coder: coder)
+          setupField()
+      }
+      
+      private func setupField() {
+          self.backgroundColor = .white
+          self.layer.borderWidth = 1
+          self.layer.cornerRadius = 12
+          self.leftViewMode = .always
+      }
+      
+      public func models(model: Model) {
+          placeholder = model.placeholder
+      }
+
 }
+
